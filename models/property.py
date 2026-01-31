3f1e64a3429f0e5ac8152d54dcf2bec1cf514c07
Jack Gledhill <me@jackgledhill.com>
2026-01-31 19:17:01 +0000

    Minor model improvements + refactor Address coordinate lookup workflow

--- models/property.py
+++ models/property.py
@@ -2,9 +2,12 @@ import datetime
 
 from .address import Address
 from sqlmodel import Field
+from pydantic import computed_field
 
 
 class Property(Address, table=True):
+    __tablename__ = "properties"
+
     id: int = Field(primary_key=True)
     bedrooms: int
     bathrooms: int
@@ -14,10 +17,17 @@ class Property(Address, table=True):
     contract_start: datetime.datetime
     contract_end: datetime.datetime
 
+    @computed_field
+    @property
+    def contract_duration(self) -> datetime.timedelta:
+        return self.contract_end - self.contract_start
+
+    @computed_field
     @property
     def rent_annual(self) -> float:
         return self.rent_weekly * 52
 
+    @computed_field
     @property
     def rent_monthly(self) -> float:
         return self.rent_annual / 12
