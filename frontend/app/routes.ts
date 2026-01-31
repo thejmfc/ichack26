7e515cd99d3fcc0d52739c9e3f33a9663d16647f
Oliver GD <114223604+OGD311@users.noreply.github.com>
2026-01-31 18:42:32 +0000

    Yeah bro it works.

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,4 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
 	index("routes/home.tsx"),
+	route("/homes/:id", "routes/homes/page.tsx"),
+	route("/signup", "routes/auth/signup.tsx"),
+	route("/signin", "routes/auth/signin.tsx"),
 ] satisfies RouteConfig;
