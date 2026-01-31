dfcd777c53688508be62c2224c5ec6ee59f7fa1c
Jack Gledhill <me@jackgledhill.com>
2026-01-31 14:48:23 +0000

    Add functions & properties for fetching lat/long from postcodes

--- models/address.py
+++ models/address.py
@@ -1,7 +1,26 @@
+import requests
+import urllib.parse
+
 from sqlmodel import SQLModel
 
+
+POSTCODES_IO_API_BASE_URL = "https://api.postcodes.io/postcodes"
 
 class Address(SQLModel):
     address_line_1: str
     address_line_2: str
     postcode: str
+
+    def get_postcode_data(self) -> dict:
+        url = f"{POSTCODES_IO_API_BASE_URL}/{urllib.parse.quote(self.postcode)}"
+        return requests.get(url).json()
+
+    @property
+    def latitude(self) -> float:
+        data = self.get_postcode_data()
+        return data["result"]["latitude"]
+
+    @property
+    def longitude(self) -> float:
+        data = self.get_postcode_data()
+        return data["result"]["longitude"]
